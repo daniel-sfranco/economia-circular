c20e5015a08139b747045e8ae8827410d382006b
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-04-09 21:38:53 -0300

    Criação inicial do banco de dados mock

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from routes import *
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mock.db'
+db = SQLAlchemy(app)
 
 app.register_blueprint(main_routes)
 
