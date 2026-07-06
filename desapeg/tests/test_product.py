314bd099ef3f13325cb0d93152eb073f95a85518
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-07-06 16:34:41 -0300

    Adicionando testes de alteração e remoção de produtos

--- desapeg/tests/test_product.py
+++ desapeg/tests/test_product.py
@@ -30,16 +30,33 @@ class ProductDBTestCase(unittest.TestCase):
                     db.session.add(Category(name=name))
             db.session.commit()
 
+            response = self.client.post(
+            '/forms',
+            data={
+                'prod_name': 'Produto Teste',
+                'description': 'Descrição muito legal',
+                'quantity': '10',
+                'price': '50.50',
+                'condition': 'Novo',
+                'usage_time': 'Nunca usado',
+                'pickup_location': 'Campinas',
+                'images': (create_test_image(), 'test.jpg'),
+                'categories': 'Móveis'
+            },
+            content_type='multipart/form-data',
+            follow_redirects=False
+        )
+
     def tearDown(self):
         with app.app_context():
             db.session.remove()
             db.drop_all()
 
-    def test_product_saved_in_db(self):
+    def test_create_product(self):
         response = self.client.post(
             '/forms',
             data={
-                'prod_name': 'Produto Teste',
+                'prod_name': 'Produto 2',
                 'description': 'Descrição muito legal',
                 'quantity': '10',
                 'price': '100.50',
@@ -58,10 +75,10 @@ class ProductDBTestCase(unittest.TestCase):
 
         # 2. Verifica o produto no banco de dados
         with app.app_context():
-            product = Product.query.first()
+            product = Product.query.order_by(Product.id.desc()).first()
 
             self.assertIsNotNone(product)
-            self.assertEqual(product.name, 'Produto Teste')
+            self.assertEqual(product.name, 'Produto 2')
             self.assertEqual(product.description, 'Descrição muito legal')
             self.assertEqual(product.quantity, 10)
             self.assertEqual(product.cost, 100.50)
@@ -69,6 +86,53 @@ class ProductDBTestCase(unittest.TestCase):
             self.assertEqual(product.usage_time, 'Nunca usado')
             self.assertEqual(product.pickup_location, 'Campinas')
             self.assertEqual([category.name for category in product.categories], ['Móveis', 'Eletrodomésticos'])
+
+    def test_update_product(self):
+        response = self.client.put(
+            '/api/product/1',
+            data={
+                'name': 'Produto 2',
+                'description': 'Descrição muito legal',
+                'condition': 'Novo',
+                'cost': '100.50',
+                'quantity': '10',
+                'usage_time': 'Nunca usado',
+                'pickup_location': 'Indaiatuba',
+                'images': (create_test_image(), 'test.jpg'),
+                'categories': 'Móveis'
+            },
+            content_type='multipart/form-data',
+            follow_redirects=False
+        )
+
+        # 1. Verifica redirect
+        self.assertEqual(response.status_code, 200)
+
+        # 2. Verifica o produto no banco de dados
+        with app.app_context():
+            product = Product.query.first()
+
+            self.assertIsNotNone(product)
+            self.assertEqual(product.name, 'Produto 2')
+            self.assertEqual(product.description, 'Descrição muito legal')
+            self.assertEqual(product.quantity, 10)
+            self.assertEqual(product.cost, 100.50)
+            self.assertEqual(product.condition, 'Novo')
+            self.assertEqual(product.usage_time, 'Nunca usado')
+            self.assertEqual(product.pickup_location, 'Indaiatuba')
+            self.assertEqual([category.name for category in product.categories], ['Móveis'])
+
+    def test_delete_product(self):
+        response = self.client.delete(
+            "api/product/1"
+        )
+
+        self.assertEqual(response.status_code, 200)
+
+        with app.app_context():
+            products = Product.query.count()
+            self.assertEqual(products, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
